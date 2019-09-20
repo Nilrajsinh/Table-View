d804119b8a09fb29bfc3eb640f33238db6d683b5
nil <Nilrajsinh6005@ICLOUD.COM>
2019-09-20 21:33:19 +0530

    done

--- Ems-Myapp/ViewController.swift
+++ Ems-Myapp/ViewController.swift
@@ -8,7 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource {
+    
+    var Menu = [
+    
+        ["Title":"Sign UP", "Controller":"RedScene"],
+        ["Title":"Sign In", "Controller":"GreenScene"]
+    
+    ]
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Menu.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell" , for: indexPath )
+        cell.textLabel?.text = Menu[indexPath.row]["Title"]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let controller = self.storyboard?.instantiateViewController(withIdentifier: Menu[indexPath.section]["Controller"]!)
+        self.navigationController?.pushViewController(controller!, animated: true)
+    }
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
